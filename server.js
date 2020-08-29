7ed128096ac6910e9299c435af96beed9e622b13
anjaneyulu786 <47409569+anjaneyulu786@users.noreply.github.com>
2020-08-29 18:32:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,12 +23,12 @@ const errorHandler = error => {
   const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
   switch (error.code) {
     case 'EACCES':
-      console.error(bind + ' requires elevated privileges.');
-      process.exit(1);
+      console.error (bind + ' requires elevated privileges.');
+      process.exit (1);
       break;
     case 'EADDRINUSE':
-      console.error(bind + ' is already in use.');
-      process.exit(1);
+      console.error (bind + ' is already in use.');
+      process.exit (1);
       break;
     default:
       throw error;
@@ -37,11 +37,11 @@ const errorHandler = error => {
 
 const server = http.createServer(app);
 
-server.on('error', errorHandler);
-server.on('listening', () => {
+server.on ('error', errorHandler);
+server.on ('listening', () => {
   const address = server.address();
   const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-  console.log('Listening on ' + bind);
+  console.log ('Listening on ' + bind);
 });
 
-server.listen(port);
+server.listen (port);
