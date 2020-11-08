1ae05f62386ccb651b7faf04f246786a21fb63cc
anjaneyulu786 <47409569+anjaneyulu786@users.noreply.github.com>
2020-11-08 16:11:23 +0530

    Update product.js

--- models/product.js
+++ models/product.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const productSchema = mongoose.Schema({
+const productSchema = mongoose.Schema ({
   name: { type: String, required: true },
   description: { type: String, required: true },
   inStock: { type: Boolean, required: true },
